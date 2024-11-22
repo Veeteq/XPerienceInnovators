a87a5545650ae56790204a08452b871f47671f95
Witold Wojnarowicz <witold.wojnarowicz@admiralhack.com>
2024-11-22 07:46:58 +0000

    workflows - azure-deploy.yml

--- src/main/java/com/admiral/hackathon/customer_data/service/AccountService.java
+++ src/main/java/com/admiral/hackathon/customer_data/service/AccountService.java
@@ -4,15 +4,19 @@ import com.admiral.hackathon.customer_data.api.dto.data.AccountDto;
 import com.admiral.hackathon.customer_data.api.mapper.AccountMapper;
 import com.admiral.hackathon.customer_data.repository.AccountRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
 
+    @Value("${azure.openai.abcde}")
+    private String abcde;
     private final AccountRepository accountRepository;
     private final AccountMapper accountMapper;
 
-    public AccountService(AccountMapper accountMapper, AccountRepository accountRepository) {
+    public AccountService(AccountMapper accountMapper,
+                          AccountRepository accountRepository) {
         this.accountMapper = accountMapper;
         this.accountRepository = accountRepository;
     }
@@ -20,6 +24,7 @@ public class AccountService {
     @Transactional
     public AccountDto findAccountByEmail(String email) {
         var account = accountRepository.findByEmail(email).get();
+        account.setName(abcde);
         return accountMapper.toDto(account);
     }
 }
